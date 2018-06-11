7dc01a2873abf4e368b4e4ddda6a21ba40144b24
lubomur <l.luzhnuy@gmail.com>
2018-06-11 18:57:40 +0300

    start develop statistick

--- main/views.py
+++ main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from minus.models import MinusstoreMinusstats, MinusstoreMinusrecord
 
 # Create your views here.
 
 def main(request):
+	minus = MinusstoreMinusstats.objects.all()
+	minus_id = minus.minus_id
+	minus_id = minus_id[:11]
+	for i in minus_id:
+		 
+
 	return render(request, 'main/index.html')
