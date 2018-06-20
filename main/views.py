a21771a25c22b8fb5770588d4dd5e04071da699b
lubomur <l.luzhnuy@gmail.com>
2018-06-20 21:31:26 +0300

    add search and dictionary

--- main/views.py
+++ main/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
-from minus.models import MinusstoreMinusstats, MinusstoreMinusrecord
+from minus.models import NewsNewsitem
 from minus.tops_functions import *
 # Create your views here.
 
 
 def main(request):
 
+	news = NewsNewsitem.objects.all()
 	
 		 
 
-	return render(request, 'main/index.html' , {'minus_top_all_time' : top_minus_per_all_time() })
+	return render(request, 'main/index.html' , {
+		'minus_top_all_time' : top_minus_per_all_time(), 
+		'minus_top_week' : top_minus_per_week(),
+		'top_users' : top_users(),
+		})
--- minus/tops_functions.py
+++ minus/tops_functions.py
@@ -1,13 +1,53 @@
-from minus.models import MinusstoreMinusstats, MinusstoreMinusrecord
+from minus.models import MinusstoreMinusweekstats, MinusstoreMinusrecord,UsersUserrating,AuthUser
 
 def top_minus_per_all_time():
-	minus = MinusstoreMinusstats.objects.all()
+	
+	
 	top_minus_per_all_time = []
-	top_minus_id = []
-	for i in range(10):	
-		top_minus_id.append(minus[i].minus_id)
+	
+	# top_minus_id = []
+	
+	# for i in range(10):	
+	# 	top_minus_id.append(minus[i].rate)
+	
+	# for i in top_minus_id:
+	minus_record = MinusstoreMinusrecord.objects.order_by('-rating_score')[:10]
+	
+	for i in minus_record:
+		top_minus_per_all_time.append(i)
 
-	for i in top_minus_id:
-		top_minus_per_all_time.append(MinusstoreMinusrecord.objects.get(pk = i))
+	return top_minus_per_all_time
 
-	return top_minus_per_all_time		
+
+def top_minus_per_week():
+
+	top_minus_per_week = []	
+
+	minus_week_stats = MinusstoreMinusweekstats.objects.order_by('-rate')[:10]
+
+	
+
+	for i in minus_week_stats:
+		top_minus_per_week.append(MinusstoreMinusrecord.objects.get(pk = i.minus_id))
+
+
+	return top_minus_per_week
+
+
+def top_users():
+
+	
+	top_users = []
+	
+	# top_minus_id = []
+	
+	# for i in range(10):	
+	# 	top_minus_id.append(minus[i].rate)
+	
+	# for i in top_minus_id:
+	users = UsersUserrating.objects.order_by('-rating')[:10]
+	
+	for i in users:
+		top_users.append(AuthUser.objects.get(pk = i.user_id))
+
+	return top_users
