12c7b786bfb24727f5dda226151599277dc2643b
lubomur <l.luzhnuy@gmail.com>
2018-06-24 21:31:52 +0300

    ((((( ))))

--- minusstore/views.py
+++ minusstore/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from minus.models import NewsNewsitem,AuthUser,DjangoComments
+from minus.models import NewsNewsitem,AuthUser,DjangoComments,MinusstoreMinusauthor
 from minus.tops_functions import *
 from django.http import HttpResponse
 from django.core import serializers
@@ -9,10 +9,13 @@ from django.core import serializers
 
 def minusstore_main(request):
 
+	author = MinusstoreMinusauthor.objects.all().order_by('name')[:20]
+
 	return render(request, 'minusstore/index.html' , {
 		'minus_top_all_time' : top_minus_per_all_time(), 
 		'minus_top_week' : top_minus_per_week(),
 		'top_users' : top_users(),
+		'author' : author,
 		
 		})
 
