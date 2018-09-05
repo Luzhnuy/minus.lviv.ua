ccf08460fd60dc74747830f2595d57ea2fb87e7a
lubomur <l.luzhnuy@gmail.com>
2018-09-05 20:08:57 +0300

    add new

--- desks/views.py
+++ desks/views.py
@@ -24,3 +24,21 @@ def desk_of_shame(request):
 		'forum' : last_forum(),
 		'form' : form,
 		})
+
+def desk_or_respect(request):
+	form = AuthForm()
+	news = NewsNewsitem.objects.all().order_by('-id')
+	for i in news:
+		i.user = AuthUser.objects.get(pk = i.user_id)
+	
+		 
+
+	return render(request, 'desks/shame.html' , {
+		'minus_top_all_time' : top_minus_per_all_time(), 
+		'minus_top_week' : top_minus_per_week(),
+		'top_users' : top_users(),
+		'news' : news,
+		'forum' : last_forum(),
+		'form' : form,
+		})
+
