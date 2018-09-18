1e8750dfdaf6b12b52443ec27fdaa38af646ad94
lubomur <l.luzhnuy@gmail.com>
2018-09-18 19:30:09 +0300

    messanger

--- minusstore/urls.py
+++ minusstore/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
   	
   	url(r'^$',views.minusstore_main, name="minusstore_main"),
-  	url(r'minus/',views.minusstore_minus, name="minus"),
+  	url(r'minus/(?P<pk>[0-9]+)/$',views.minusstore_minus, name="minus"),
   	url(r'add_minus/',views.add_minus, name="add_minus"),
 
 ]
--- minusstore/views.py
+++ minusstore/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from minus.models import NewsNewsitem,AuthUser,DjangoComments,MinusstoreMinusauthor
+from django.shortcuts import render,get_object_or_404
+from minus.models import NewsNewsitem,AuthUser,DjangoComments,MinusstoreMinusauthor,MinusstoreMinusrecord
 from django.http import HttpResponse
 from django.core import serializers
 from main.forms import AuthForm
@@ -20,13 +20,21 @@ def minusstore_main(request):
 		})
 
 
-def minusstore_minus(request):
+def minusstore_minus(request,pk):
 		form = AuthForm(request.POST)
 		signin(request,form)
-
-
+		minus = get_object_or_404(MinusstoreMinusrecord,pk=pk)
+		author = get_object_or_404(MinusstoreMinusauthor,pk=minus.author_id)
+		comments = DjangoComments.objects.filter(object_pk=minus.pk,content_type_id=17)
+		minus_user = get_object_or_404(AuthUser,pk=minus.user_id)
+		upload_minuses_from_user = MinusstoreMinusrecord.objects.filter(user_id=minus_user.id).count()
 		return render(request, 'minusstore/minus.html' , {
 		'usr' : request.session['usr'],
+		'minus' : minus,
+		'author' : author,
+		'comments' : comments,
+		'minus_user':minus_user,
+		'upload_minuses' : upload_minuses_from_user,
 		})
 
 def add_minus(request):
