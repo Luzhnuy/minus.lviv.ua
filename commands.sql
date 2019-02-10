20116d9235b3e14e31836177172915628b806d55
Lubas <l.luzhnuy@gmail.com>
2019-02-10 12:24:15 +0200

    fix some problem with interface

--- commands.sql
+++ commands.sql
@@ -1,8 +1,9 @@
-ALTER TABLE userprofile  ADD COLUMN is_business INT DAFAULT 0;
-ALTER TABLE blurbs_blurb  ADD COLUMN is_user_business INT DAFAULT 0;
+USE minus;
+ALTER TABLE userprofile  ADD COLUMN is_business INT NOT NULL DEFAULT 0;
+ALTER TABLE blurbs_blurb  ADD COLUMN is_user_business INT DEFAULT 0;
 UPDATE userprofile SET is_business = 0;
 UPDATE blurbs_blurb SET is_user_business = 0;
-ALTER TABLE userprofile MODIFY hide_birthdate date DEFAULT 0;
+ALTER TABLE userprofile MODIFY hide_birthdate TINYINT(1) DEFAULT 0;
 ALTER TABLE userprofile MODIFY is_admin_subscribed INT DEFAULT 0;
 ALTER DATABASE minus CHARACTER SET utf8;
 ALTER TABLE userprofile ADD COLUMN is_user_online TINYINT(1) default 0;
@@ -12,7 +13,3 @@ ALTER TABLE minusstore_minusrecord ADD COLUMN record_quality_score INT;
 ALTER TABLE minusstore_minusrecord ADD COLUMN arrangement_score INT;
 ALTER TABLE minusstore_minusrecord ALTER COLUMN record_quality_score SET DEFAULT 0;
 ALTER TABLE minusstore_minusrecord ALTER COLUMN arrangement_score SET DEFAULT 0;
-
-
-
-Установка redis server
